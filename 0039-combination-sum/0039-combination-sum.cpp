b28b7084b09d4f9ec8e5a7765f1029d405b5b5a3
KALPIT NAGAR <kalpitnagar312@gmail.com>
2026-07-05 22:33:41 +0530

    Time: 0 ms (100%), Space: 14 MB (75.94%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
-    void findComb(int index, int target, vector<int>& candidates,vector<vector<int>> &ans, vector<int> ds){
-       if(index==candidates.size()) {
-        if(target==0) {ans.push_back(ds);
-       }
-        return;
-       }
 
-       // pick 
-       if(candidates[index]<=target){
-        ds.push_back(candidates[index]);
-        findComb(index,target-candidates[index],candidates,ans,ds);
-        ds.pop_back();
-       }
+    void findComb(int index,
+                  int target,
+                  vector<int>& candidates,
+                  vector<vector<int>> &ans,
+                  vector<int> &ds)
+    {
+        // Base Case
+        if(index == candidates.size())
+        {
+            if(target == 0)
+                ans.push_back(ds);
 
-       // not pick leave it
-       findComb(index+1,target,candidates,ans,ds);
+            return;
+        }
 
+        // Pick
+        if(candidates[index] <= target)
+        {
+            ds.push_back(candidates[index]);
+
+            // Stay at same index (reuse allowed)
+            findComb(index,
+                     target - candidates[index],
+                     candidates,
+                     ans,
+                     ds);
+
+            ds.pop_back();
+        }
+
+        // Not Pick
+        findComb(index + 1,
+                 target,
+                 candidates,
+                 ans,
+                 ds);
     }
-        vector<vector<int>> combinationSum(vector<int>& candidates,int target) {
+
+    vector<vector<int>> combinationSum(vector<int>& candidates,
+                                       int target)
+    {
         vector<vector<int>> ans;
         vector<int> ds;
+
         findComb(0, target, candidates, ans, ds);
+
         return ans;
     }
 };
