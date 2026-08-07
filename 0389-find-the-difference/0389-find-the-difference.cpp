646b3dc1e33d629e0afddef1ace0a73de27469f6
KALPIT NAGAR <kalpitnagar312@gmail.com>
2026-08-08 02:25:21 +0530

    Time: 0 ms (100%), Space: 9.3 MB (25.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int ss[256] = {0};
-        int tt[256] = {0};
-        char ans='\0';
-       
-        for(int i=0;i<t.size();i++){
-            if(i<s.size()) ss[s[i]]++;
-            tt[t[i]]++;
+        char ans = 0;
+
+        for (char ch : s) {
+            ans ^= ch;
         }
-        for(int i=0;i<256;i++){
-            if(tt[i]!=ss[i]) ans = (char)i;
+
+        for (char ch : t) {
+            ans ^= ch;
         }
-        
+
         return ans;
     }
 };
