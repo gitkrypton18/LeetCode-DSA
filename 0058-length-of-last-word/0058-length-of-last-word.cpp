b0224551a801a4f7ed18fc75e0a7bcdd4efedb97
KALPIT NAGAR <kalpitnagar312@gmail.com>
2026-08-20 23:48:16 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.67%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int cnt=0;
+        int i=n-1;
+        
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            cnt++;
+            i--;
+        }
+            
+
+
+        
+        return cnt;
+    }
+};
